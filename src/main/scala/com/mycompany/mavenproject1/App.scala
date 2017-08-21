16e1663a7be48e7b31ff31caa5c1683b61b7f731
hamza6348 <hamzanasim6348@gmail.com>
2017-08-21 12:13:03 +0500

    s

--- src/main/scala/com/mycompany/mavenproject1/App.scala
+++ src/main/scala/com/mycompany/mavenproject1/App.scala
@@ -6,7 +6,32 @@ package com.mycompany.mavenproject1
  */
 object App 
 {
+  
+  
+  /*A huffman code  is represented by a binary tree
+   * 
+   * 
+   * 
+   * Lo bhai
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   * 
+   */*/
+  
+  
+  
+  
+  
+  
+  
     def main(args: Array[String]):Unit={
-        println( "Hello World!" )
+        println( "Hello World! Hn g kaise ho" )
     }
 }
